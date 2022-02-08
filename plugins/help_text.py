b6b2c38b6c6a81c439707089b22ac04cee2fb4f0
VIP_Ridhan <92419707+MoneyEarnVip@users.noreply.github.com>
2022-02-08 12:12:18 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -46,11 +46,11 @@ async def start(bot, update):
             [
                 [
                     InlineKeyboardButton(
-                        "Source code ⚡", url="https://github.com/Clinton-Abraham/UPLOADER-BOT"
+                        "Source code ⚡", url="https://github.com/MoneyEarnVip/UPLOADER-BOT"
                     ),
-                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/Space_X_bots"),
+                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/TamilanXbots"),
                 ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham")],
+                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/Tamilanbots")],
             ]
         ),
         reply_to_message_id=update.message_id
