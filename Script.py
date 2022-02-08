2c870f633c8c072ea5fdd3edbc9451ba1fad4068
VIP_Ridhan <92419707+MoneyEarnVip@users.noreply.github.com>
2022-02-08 18:34:02 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,30 @@
+class script(object):
+
+    HELP_TEXT = """𝙷𝙴𝚈 {}
+ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ? ғᴏʟʟᴏᴡ ᴛʜᴇsᴇ sᴛᴇᴘs!
+    
+1. sᴇɴᴅ ᴜʀʟ (ᴇxᴀᴍᴘʟᴇ.ᴅᴏᴍᴀɪɴ/ғɪʟᴇ.ᴍᴘ4 | ɴᴇᴡ ғɪʟᴇɴᴀᴍᴇ.ᴍᴘ4).
+2. sᴇɴᴅ ɪᴍᴀɢᴇ ᴀs ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ (ᴏᴘᴛɪᴏɴᴀʟ).
+3. sᴇʟᴇᴄᴛ ᴛʜᴇ ʙᴜᴛᴛᴏɴ.
+   sᴠɪᴅᴇᴏ - ɢɪᴠᴇ ғɪʟᴇ ᴀs ᴠɪᴅᴇᴏ ᴡɪᴛʜ sᴄʀᴇᴇɴsʜᴏᴛs
+   ᴅғɪʟᴇ  - ɢɪᴠᴇ ғɪʟᴇ (ᴠɪᴅᴇᴏ) ᴀs ғɪʟᴇ ᴡɪᴛʜ sᴄʀᴇᴇɴsʜᴏᴛs
+   ᴠɪᴅᴇᴏ  - ɢɪᴠᴇ ғɪʟᴇ ᴀs ᴠɪᴅᴇᴏ ᴡɪᴛʜᴏᴜᴛ sᴄʀᴇᴇɴsʜᴏᴛs
+   ғɪʟᴇ   - ɢɪᴠᴇ ғɪʟᴇ ᴡɪᴛʜᴏᴜᴛ sᴄʀᴇᴇɴsʜᴏᴛs
+ɪғ ʙᴏᴛ ᴅɪᴅɴ'ᴛ ʀᴇsᴘᴏɴᴅ, ᴄᴏɴᴛᴀᴄᴛ [@ᴛᴀᴍɪʟᴀɴxʙᴏᴛs](https://t.me/TamilanXBots)"""
+    ABOUT_TXT = """✯ Mʏ ɴᴀᴍᴇ : [ᴜᴘʟᴏᴀᴅᴇʀ x ʙᴏᴛ](https://t.me/UploaderrXRobot)
+
+✯ Cʜᴀɴɴᴇʟ : [ᴛᴀᴍɪʟᴀɴ x ʙᴏᴛs](https://t.me/TamilanXBots)
+
+✯ Vᴇʀꜱɪᴏɴ : [2.0 ʙᴇᴛᴀ](https://t.me/UploaderrXRobot)
+
+✯ Sᴏᴜʀᴄᴇ : [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/tamilanbots)
+
+✯ Sᴇʀᴠᴇʀ : [ʜᴇʀᴏᴋᴜ](https://heroku.com/)
+
+✯ Lᴀɴɢᴜᴀɢᴇ : [Pʏᴛʜᴏɴ 3.10.2](https://www.python.org/)
+
+✯Fʀᴀᴍᴇᴡᴏʀᴋ : [ᴘʏʀᴏɢᴀᴍ 1.2.9](https://docs.pyrogram.org/)
+
+✯Dᴇᴠᴇʟᴏᴘᴇʀ : [𝙼𝚘𝚗𝚎𝚢𝙴𝚊𝚛𝚗𝚅𝙸𝙿](https://t.me/tamilanbots)
+
+✯Pᴏᴡᴇʀᴇᴅ ʙʏ : [@ᴅɪsɴᴇʏʜᴅʟɪɴᴋs](https://t.me/DisneyHDLinks)"""
