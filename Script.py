af088a38d0b54e8699aecbdc844732a3e5023a23
VIP_Ridhan <92419707+MoneyEarnVip@users.noreply.github.com>
2022-02-08 19:29:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,4 +1,22 @@
 class script(object):
+START_TXT = """👋 Hᴇʟʟᴏ , {}♡
+Tʜɪꜱ ɪꜱ ᴘᴏᴡᴇʀꜰᴜʟ ᴜʀʟ ᴜᴘʟᴏᴀᴅᴇʀ ʙᴏᴛ.
+Pʀᴇꜱꜱ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ...
+Pᴏᴡᴇʀᴇᴅ ʙʏ : [ᴛᴀᴍɪʟᴀɴ x ʙᴏᴛs](https://t.me/TamilanXBots)"""
+
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    SET_CUSTOM_USERNAME_PASSWORD = """ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴘʀᴇᴍɪᴜᴍ ᴠɪᴅᴇᴏs, ᴘʀᴏᴠɪᴅᴇ ɪɴ ᴛʜᴇ ғᴏʟʟᴏᴡɪɴɢ ғᴏʀᴍᴀᴛ:
+URL | filename | username | password"""
+    DOWNLOAD_START = "📥ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ..."
+    UPLOAD_START = "📤ᴜᴘʟᴏᴀᴅɪɴɢ..."
+    RCHD_TG_API_LIMIT = "ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\nᴅᴇᴛᴇᴄᴛᴇᴅ ғɪʟᴇ sɪᴢᴇ : {}\nsᴏʀʀʏ. ʙᴜᴛ, ɪ ᴄᴀɴɴᴏᴛ ᴜᴘʟᴏᴀᴅ ғɪʟᴇs ɢʀᴇᴀᴛᴇʀ ᴛʜᴀɴ 2ɢʙ due ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴘɪ ʟɪᴍɪᴛᴀᴛɪᴏɴ."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ @UploaderrXRobot\n\n<b>Join : @TamilanXbots</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = " ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\nᴜᴘʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\n\n@UploaderrXRobot"
+    SAVED_CUSTOM_THUMB_NAIL = "ᴄᴜsᴛᴏᴍ ᴠɪᴅᴇᴏ / ғɪʟᴇ ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ. This image will be used in the video / file."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴄʟᴇᴀʀᴇᴅ sᴜᴄᴄᴇsғᴜʟʟʏ."
+    CUSTOM_CAPTION_UL_FILE = "{}"
+    NO_VOID_FORMAT_FOUND = "Pʟᴇᴀꜱᴇ ꜱᴇɴᴅ ᴀ ᴠᴀʟɪᴅ ᴜʀʟ"
+    CANCEL_STR = "Process Cancelled"
 
     HELP_TXT = """𝙷𝙴𝚈 {}
 ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ? ғᴏʟʟᴏᴡ ᴛʜᴇsᴇ sᴛᴇᴘs!
