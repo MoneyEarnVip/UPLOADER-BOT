ed38b00d5aab376f734448b662ec3f4915a989ed
VIP_Ridhan <92419707+MoneyEarnVip@users.noreply.github.com>
2022-02-09 14:08:32 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,20 +5,6 @@ Tʜɪꜱ ɪꜱ ᴘᴏᴡᴇʀꜰᴜʟ ᴜʀʟ ᴜᴘʟᴏᴀᴅᴇʀ ʙᴏᴛ.
 Pʀᴇꜱꜱ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ...
 
 Pᴏᴡᴇʀᴇᴅ ʙʏ : [ᴛᴀᴍɪʟᴀɴ x ʙᴏᴛs](https://t.me/TamilanXBots)"""
-
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-    SET_CUSTOM_USERNAME_PASSWORD = """ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴘʀᴇᴍɪᴜᴍ ᴠɪᴅᴇᴏs, ᴘʀᴏᴠɪᴅᴇ ɪɴ ᴛʜᴇ ғᴏʟʟᴏᴡɪɴɢ ғᴏʀᴍᴀᴛ:
-URL | filename | username | password"""
-    DOWNLOAD_START = "📥ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ..."
-    UPLOAD_START = "📤ᴜᴘʟᴏᴀᴅɪɴɢ..."
-    RCHD_TG_API_LIMIT = "ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\nᴅᴇᴛᴇᴄᴛᴇᴅ ғɪʟᴇ sɪᴢᴇ : {}\nsᴏʀʀʏ. ʙᴜᴛ, ɪ ᴄᴀɴɴᴏᴛ ᴜᴘʟᴏᴀᴅ ғɪʟᴇs ɢʀᴇᴀᴛᴇʀ ᴛʜᴀɴ 2ɢʙ due ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴘɪ ʟɪᴍɪᴛᴀᴛɪᴏɴ."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ @UploaderrXRobot\n\n<b>Join : @TamilanXbots</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = " ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\nᴜᴘʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\n\n@UploaderrXRobot"
-    SAVED_CUSTOM_THUMB_NAIL = "ᴄᴜsᴛᴏᴍ ᴠɪᴅᴇᴏ / ғɪʟᴇ ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴄʟᴇᴀʀᴇᴅ sᴜᴄᴄᴇsғᴜʟʟʏ."
-    CUSTOM_CAPTION_UL_FILE = "{}"
-    NO_VOID_FORMAT_FOUND = "Pʟᴇᴀꜱᴇ ꜱᴇɴᴅ ᴀ ᴠᴀʟɪᴅ ᴜʀʟ"
-    CANCEL_STR = "Process Cancelled"
     HELP_TEXT = """𝙷𝙴𝚈 {}
 ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ? ғᴏʟʟᴏᴡ ᴛʜᴇsᴇ sᴛᴇᴘs!
     
@@ -56,3 +42,17 @@ URL | filename | username | password"""
 
 • ᴜᴘʟᴏᴀᴅᴇʀ x ʙᴏᴛ ɪs ᴀ ᴄʟᴏsᴇᴅ sᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ.
 • ᴄᴏɴᴛᴀᴄᴛ ᴛᴀᴍɪʟᴀɴ x ʙᴏᴛs ᴏɴʟʏ ғᴏʀ ʀᴇᴘᴏʀᴛɪɴɢ ʙᴜɢs."""
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    SET_CUSTOM_USERNAME_PASSWORD = """ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴘʀᴇᴍɪᴜᴍ ᴠɪᴅᴇᴏs, ᴘʀᴏᴠɪᴅᴇ ɪɴ ᴛʜᴇ ғᴏʟʟᴏᴡɪɴɢ ғᴏʀᴍᴀᴛ:
+URL | filename | username | password"""
+    DOWNLOAD_START = "📥ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ..."
+    UPLOAD_START = "📤ᴜᴘʟᴏᴀᴅɪɴɢ..."
+    RCHD_TG_API_LIMIT = "ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\nᴅᴇᴛᴇᴄᴛᴇᴅ ғɪʟᴇ sɪᴢᴇ : {}\nsᴏʀʀʏ. ʙᴜᴛ, ɪ ᴄᴀɴɴᴏᴛ ᴜᴘʟᴏᴀᴅ ғɪʟᴇs ɢʀᴇᴀᴛᴇʀ ᴛʜᴀɴ 2ɢʙ due ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴘɪ ʟɪᴍɪᴛᴀᴛɪᴏɴ."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "ᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ @UploaderrXRobot\n\n<b>Join : @TamilanXbots</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = " ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\nᴜᴘʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\n\n@UploaderrXRobot"
+    SAVED_CUSTOM_THUMB_NAIL = "ᴄᴜsᴛᴏᴍ ᴠɪᴅᴇᴏ / ғɪʟᴇ ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ. This image will be used in the video / file."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴄʟᴇᴀʀᴇᴅ sᴜᴄᴄᴇsғᴜʟʟʏ."
+    CUSTOM_CAPTION_UL_FILE = "{}"
+    NO_VOID_FORMAT_FOUND = "Pʟᴇᴀꜱᴇ ꜱᴇɴᴅ ᴀ ᴠᴀʟɪᴅ ᴜʀʟ"
+    CANCEL_STR = "Process Cancelled"
+    
